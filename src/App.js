1e54a122f2d96fbfed40c909ec2796c5cf8fa0a9
Viacheslav Severyn <sever_2008@ukr.net>
2022-06-11 13:23:51 +0300

    start_config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div className={s.app}></div>;
+  }
+}
+
+export default App;
